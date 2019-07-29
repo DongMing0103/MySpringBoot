baaee5ff9bb90759e8985754fedeee6a89d4079d
DongMing0103 <316588010@qq.com>
2019-07-29 17:26:21 +0800

    add 增加selectPageByVo 根据vo进行分页查询

--- src/main/java/com/example/demo/mapper/JFTestMapper.java
+++ src/main/java/com/example/demo/mapper/JFTestMapper.java
@@ -1,13 +1,17 @@
 package com.example.demo.mapper;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.example.demo.entity.po.JFTest;
+import com.example.demo.entity.vo.JFTestVo;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * <p>
- *  Mapper 接口
+ * Mapper 接口
  * </p>
  *
  * @author dongm
@@ -16,5 +20,11 @@ import org.springframework.stereotype.Repository;
 @Mapper
 @Repository
 public interface JFTestMapper extends BaseMapper<JFTest> {
-
+    /**
+     * @author dongm
+     * @Description：根据vo查询分页数据
+     * @date：2019/7/29 15:52
+     * @param:
+     */
+    List<JFTest> selectPageByVo(Page<JFTest> page, JFTestVo vo);
 }
