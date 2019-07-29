0d6f9255d7a504348a37b4e5635105fef52316cd
DongMing0103 <316588010@qq.com>
2019-07-29 17:24:59 +0800

    add 增加selectPageByVo 根据vo进行分页查询

--- src/main/java/com/example/demo/service/IJFTestService.java
+++ src/main/java/com/example/demo/service/IJFTestService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.IService;
 import com.example.demo.entity.po.JFTest;
 import com.example.demo.entity.vo.JFTestVo;
@@ -16,4 +17,5 @@ public interface IJFTestService extends IService<JFTest> {
     void save(JFTestVo vo);
     void update (JFTestVo vo);
     void delete (JFTestVo vo);
+    Page<JFTest> selectPageByVo(JFTestVo vo);
 }
