23051834e6fbd2bba63937baffbca0a4b986bb0b
DongMing0103 <316588010@qq.com>
2019-07-30 17:31:41 +0800

    update 增加throws BusinessException 异常抛出

--- src/main/java/com/example/demo/service/IJFTestService.java
+++ src/main/java/com/example/demo/service/IJFTestService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.IService;
 import com.example.demo.entity.po.JFTest;
 import com.example.demo.entity.vo.JFTestVo;
+import com.example.demo.exception.BusinessException;
 
 /**
  * <p>
@@ -14,8 +15,8 @@ import com.example.demo.entity.vo.JFTestVo;
  * @since 2019-07-26
  */
 public interface IJFTestService extends IService<JFTest> {
-    void save(JFTestVo vo);
+    void save(JFTestVo vo) throws BusinessException;
     void update (JFTestVo vo);
     void delete (JFTestVo vo);
-    Page<JFTest> selectPageByVo(JFTestVo vo);
+    Page<JFTest> selectPageByVo(JFTestVo vo) throws BusinessException;
 }
